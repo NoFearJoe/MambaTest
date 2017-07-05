5e3658e2333ed50d5c082e613f4b4a3836056a19
IlyaKharabet <ilyakharabet@gmail.com>
2017-07-05 11:02:35 +0300

    Initial commit

---
+++ MambaTest/MambaTest/MVVMModule/View.swift
@@ -0,0 +1,9 @@
+//
+//  View.swift
+//  MambaTest
+//
+//  Created by Ilya Kharabet on 05.07.17.
+//  Copyright © 2017 IK. All rights reserved.
+//
+
+import class UIKit.UIViewController
---
+++ MambaTest/MambaTest/MVVMModule/ViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  ViewModel.swift
+//  MambaTest
+//
+//  Created by Ilya Kharabet on 05.07.17.
+//  Copyright © 2017 IK. All rights reserved.
+//
+
+import Foundation
